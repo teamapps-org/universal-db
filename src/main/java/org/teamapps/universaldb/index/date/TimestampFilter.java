60546d97e20e2b8359455d80841c8861ae893ca7
Matthias Bernstein <matthias.bernstein@teamapps.org>
2020-06-03 00:16:05 +0200

    new timestamp filter

--- src/main/java/org/teamapps/universaldb/index/date/TimestampFilter.java
+++ src/main/java/org/teamapps/universaldb/index/date/TimestampFilter.java
@@ -22,47 +22,69 @@ package org.teamapps.universaldb.index.date;
 import org.teamapps.universaldb.index.numeric.NumericFilter;
 import org.teamapps.universaldb.index.numeric.NumericFilterType;
 
+import java.time.*;
 import java.util.List;
 
 public class TimestampFilter extends NumericFilter{
 
-	/*
-		AsSeconds
-		AsMilliSeconds
-		AsInstant
-		AsLocalDateTime
-	 */
-
-	public static TimestampFilter createEqualsFilter(Number number) {
-		return new TimestampFilter(NumericFilterType.EQUALS, number, null);
+	public static TimestampFilter createGreaterFilter(LocalDate date) {
+		return new TimestampFilter(NumericFilterType.GREATER, convertToEpochSecond(date), null);
 	}
 
-	public static TimestampFilter createNotEqualsFilter(Number number) {
-		return new TimestampFilter(NumericFilterType.NOT_EQUALS, number, null);
+	public static TimestampFilter createGreaterFilter(LocalDateTime date) {
+		return new TimestampFilter(NumericFilterType.GREATER, convertToEpochSecond(date), null);
 	}
 
-	public static TimestampFilter createGreaterFilter(Number number) {
-		return new TimestampFilter(NumericFilterType.GREATER, number, null);
+	public static TimestampFilter createGreaterFilter(Instant date) {
+		return new TimestampFilter(NumericFilterType.GREATER, convertToEpochSecond(date), null);
 	}
 
-	public static TimestampFilter createGreaterEqualsFilter(Number number) {
-		return new TimestampFilter(NumericFilterType.GREATER_EQUALS, number, null);
+	public static TimestampFilter createGreaterEqualsFilter(LocalDate date) {
+		return new TimestampFilter(NumericFilterType.GREATER_EQUALS, convertToEpochSecond(date), null);
 	}
 
-	public static TimestampFilter createSmallerFilter(Number number) {
-		return new TimestampFilter(NumericFilterType.SMALLER, number, null);
+	public static TimestampFilter createGreaterEqualsFilter(LocalDateTime date) {
+		return new TimestampFilter(NumericFilterType.GREATER_EQUALS, convertToEpochSecond(date), null);
 	}
 
-	public static TimestampFilter createSmallerEqualsFilter(Number number) {
-		return new TimestampFilter(NumericFilterType.SMALLER_EQUALS, number, null);
+	public static TimestampFilter createGreaterEqualsFilter(Instant date) {
+		return new TimestampFilter(NumericFilterType.GREATER_EQUALS, convertToEpochSecond(date), null);
 	}
 
-	public static TimestampFilter createBetweenFilter(Number number1, Number number2) {
-		return new TimestampFilter(NumericFilterType.BETWEEN, number1, number2);
+	public static TimestampFilter createSmallerFilter(LocalDate date) {
+		return new TimestampFilter(NumericFilterType.SMALLER, convertToEpochSecond(date), null);
 	}
 
-	public static TimestampFilter createBetweenExclusiveFilter(Number number1, Number number2) {
-		return new TimestampFilter(NumericFilterType.BETWEEN_EXCLUSIVE, number1, number2);
+	public static TimestampFilter createSmallerFilter(LocalDateTime date) {
+		return new TimestampFilter(NumericFilterType.SMALLER, convertToEpochSecond(date), null);
+	}
+
+	public static TimestampFilter createSmallerFilter(Instant date) {
+		return new TimestampFilter(NumericFilterType.SMALLER, convertToEpochSecond(date), null);
+	}
+
+	public static TimestampFilter createSmallerEqualsFilter(LocalDate date) {
+		return new TimestampFilter(NumericFilterType.SMALLER_EQUALS, convertToEpochSecond(date), null);
+	}
+
+	public static TimestampFilter createSmallerEqualsFilter(LocalDateTime date) {
+		return new TimestampFilter(NumericFilterType.SMALLER_EQUALS, convertToEpochSecond(date), null);
+	}
+
+	public static TimestampFilter createSmallerEqualsFilter(Instant date) {
+		return new TimestampFilter(NumericFilterType.SMALLER_EQUALS, convertToEpochSecond(date), null);
+	}
+
+	public static TimestampFilter createBetweenFilter(LocalDate date1, LocalDate date2) {
+		return new TimestampFilter(NumericFilterType.BETWEEN, convertToEpochSecond(date1), convertToEpochSecond(date2));
+	}
+
+	public static TimestampFilter createBetweenFilter(LocalDateTime date1, LocalDateTime date2) {
+		return new TimestampFilter(NumericFilterType.BETWEEN, convertToEpochSecond(date1), convertToEpochSecond(date2));
+	}
+
+	public static TimestampFilter createBetweenFilter(Instant date1, Instant date2) {
+		return new TimestampFilter(NumericFilterType.BETWEEN, convertToEpochSecond(date1), convertToEpochSecond(date2));
 	}
 
 	protected TimestampFilter(NumericFilterType filterType, List<Number> values) {
@@ -71,6 +93,18 @@ public class TimestampFilter extends NumericFilter{
 
 	protected TimestampFilter(NumericFilterType filterType, Number value1, Number value2) {
 		super(filterType, value1, value2);
+	}
+
+	private static int convertToEpochSecond(LocalDate date) {
+		return (int) date.toEpochSecond(LocalTime.MIDNIGHT, ZoneOffset.UTC);
+	}
+
+	private static int convertToEpochSecond(LocalDateTime dateTime) {
+		return (int) dateTime.toEpochSecond(ZoneOffset.UTC);
+	}
+
+	private static int convertToEpochSecond(Instant instant) {
+		return (int) (instant.toEpochMilli() / 1000);
 	}
 
 
