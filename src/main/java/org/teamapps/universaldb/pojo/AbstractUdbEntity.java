47587bcffdad6d6ec194f15a234e38b6872e89c7
Matthias Bernstein <matthias.bernstein@teamapps.org>
2021-07-06 20:48:59 +0200

    fixed single reference update check

--- src/main/java/org/teamapps/universaldb/pojo/AbstractUdbEntity.java
+++ src/main/java/org/teamapps/universaldb/pojo/AbstractUdbEntity.java
@@ -184,7 +184,7 @@ public abstract class AbstractUdbEntity<ENTITY extends Entity> implements Entity
 			recordReference = new RecordReference(entity.getId(), entity.getCorrelationId());
 		}
 		int currentValue = index.getValue(getId());
-		if ((currentValue == 0 && entity != null) || (entity != null && (entity.getId() == 0 || entity.getId() != currentValue))) {
+		if ((currentValue == 0 && entity != null) || (currentValue > 0 && entity == null) || (entity != null && (entity.getId() == 0 || entity.getId() != currentValue))) {
 			checkChangeSet();
 			entityChangeSet.addChangeValue(index, recordReference);
 			entityChangeSet.setReferenceChange(index, entity);
